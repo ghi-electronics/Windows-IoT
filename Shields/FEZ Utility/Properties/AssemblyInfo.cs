12e703002aa67003cff749e932c078529826dfcc
John <john.brochue@ghielectronics.com>
2015-09-03 13:21:07 -0400

    Changed references to nuget.

--- Shields/FEZ Utility/Properties/AssemblyInfo.cs
+++ Shields/FEZ Utility/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("UWP driver for the FEZ Utility.")]
 [assembly: AssemblyCompany("GHI Electronics, LLC")]
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
 [assembly: ComVisible(false)]
