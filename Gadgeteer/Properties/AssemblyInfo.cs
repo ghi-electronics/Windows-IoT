42a222fc206c3fa45f09e227335bebe1ad0e4236
John Brochue <john.brochue@ghielectronics.com>
2015-11-17 14:26:41 -0500

    Updated versions.

--- Gadgeteer/Properties/AssemblyInfo.cs
+++ Gadgeteer/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("UWP drivers for GHI Electronics Gadgeteer modules and mainboards.")]
 [assembly: AssemblyCompany("GHI Electronics, LLC")]
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
-[assembly: AssemblyVersion("0.0.6.0")]
-[assembly: AssemblyFileVersion("0.0.6.0")]
+[assembly: AssemblyVersion("0.0.8.0")]
+[assembly: AssemblyFileVersion("0.0.8.0")]
 [assembly: ComVisible(false)]
