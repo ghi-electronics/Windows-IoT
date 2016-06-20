7f31f61769d3aad2e69d7efee7db050dcd9d77eb
John Brochue <john.brochue@ghielectronics.com>
2016-06-20 13:02:33 -0400

    Added ReadRaw to ADS7830.

--- Drivers/ADS7830.cs
+++ Drivers/ADS7830.cs
@@ -2,42 +2,44 @@
 using Windows.Devices.I2c;
 
 namespace GHIElectronics.UWP.LowLevelDrivers {
-	public class ADS7830 {
-		private I2cDevice device;
-		private bool disposed;
-		private byte[] read;
-		private byte[] write;
+    public class ADS7830 {
+        private I2cDevice device;
+        private bool disposed;
+        private byte[] read;
+        private byte[] write;
 
-		public static byte GetAddress(bool a0, bool a1) => (byte)(0x48 | (a0 ? 1 : 0) | (a1 ? 2 : 0));
+        public static byte GetAddress(bool a0, bool a1) => (byte)(0x48 | (a0 ? 1 : 0) | (a1 ? 2 : 0));
 
-		public void Dispose() => this.Dispose(true);
+        public void Dispose() => this.Dispose(true);
 
-		public ADS7830(I2cDevice device) {
-			this.device = device;
-			this.disposed = false;
-			this.read = new byte[1];
-			this.write = new byte[1];
-		}
+        public ADS7830(I2cDevice device) {
+            this.device = device;
+            this.disposed = false;
+            this.read = new byte[1];
+            this.write = new byte[1];
+        }
 
-		protected virtual void Dispose(bool disposing) {
-			if (!this.disposed) {
-				if (disposing) {
-					this.device.Dispose();
-				}
+        protected virtual void Dispose(bool disposing) {
+            if (!this.disposed) {
+                if (disposing) {
+                    this.device.Dispose();
+                }
 
-				this.disposed = true;
-			}
-		}
+                this.disposed = true;
+            }
+        }
 
-		public double Read(int channel) {
-			if (this.disposed) throw new ObjectDisposedException(nameof(ADS7830));
-			if (channel > 8 || channel < 0) throw new ArgumentOutOfRangeException(nameof(channel));
+        public int ReadRaw(int channel) {
+            if (this.disposed) throw new ObjectDisposedException(nameof(ADS7830));
+            if (channel > 8 || channel < 0) throw new ArgumentOutOfRangeException(nameof(channel));
 
-			this.write[0] = (byte)(0x84 | ((channel % 2 == 0 ? channel / 2 : (channel - 1) / 2 + 4) << 4));
+            this.write[0] = (byte)(0x84 | ((channel % 2 == 0 ? channel / 2 : (channel - 1) / 2 + 4) << 4));
 
-			this.device.WriteRead(this.write, this.read);
+            this.device.WriteRead(this.write, this.read);
 
-			return this.read[0] / 255.0;
-		}
-	}
+            return this.read[0];
+        }
+
+        public double Read(int channel) => this.ReadRaw(channel) / 255.0;
+    }
 }
